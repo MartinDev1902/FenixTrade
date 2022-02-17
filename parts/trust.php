be42fd4be01c273ea8ceb4e773e2bde9efb2f2f1
NewMartin1902 <martin.new1902@gmail.com>
2022-02-17 11:55:21 +0200

    Ready trust section

---
+++ parts/trust.php
@@ -0,0 +1,49 @@
+<section class="section trust">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title text-center content-block animate zoom-in"> 
+                    <h2>Trade with <span class="accent-text">Trust</span></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row trust__items align-items-start">
+            <div class="col-3 text-center">
+                <div class="trust__item content-block animate fade-up">
+                    <div class="trust__icon d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/trust/1.png" alt="">
+                    </div>
+                    <h3>Authorised and regulated</h3>
+                    <p>Fenix Trade is authorised and regulated by the Cyprus Securities and Exchange Commission (Licence No. 250/14)</p>
+                </div>
+            </div>
+            <div class="col-3 text-center">
+                <div class="trust__item content-block animate fade-up">
+                    <div class="trust__icon d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/trust/2.png" alt="">
+                    </div>
+                    <h3>Listed on the LSE Main Market</h3>
+                    <p>Fenix Trade is a FTSE 250 company listed on the London Stock Exchange’s Main Market for Listed Companies.</p>
+                </div>
+            </div>
+            <div class="col-3 text-center">
+                <div class="trust__item content-block animate fade-up">
+                    <div class="trust__icon d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/trust/3.png" alt="">
+                    </div>
+                    <h3>Protection of funds</h3>
+                    <p>Your funds are kept in segregated bank accounts.</p>
+                </div>
+            </div>
+            <div class="col-3 text-center">
+                <div class="trust__item content-block animate fade-up">
+                    <div class="trust__icon d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/trust/4.png" alt="">
+                    </div>
+                    <h3>SSL certificate</h3>
+                    <p>Secured by SSL.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
