d09972f3c6d24656f2bbb8a93dc504d25f17028e
NewMartin1902 <martin.new1902@gmail.com>
2022-02-17 10:16:18 +0200

    Ready Fingertips section

--- front-page.php
+++ front-page.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
 <?php get_template_part('parts/main-slider'); ?>
+<?php get_template_part('parts/fingertips'); ?>
+<?php get_template_part('parts/trust'); ?>
 <?php get_footer(); ?> 
     
---
+++ parts/fingertips.php
@@ -0,0 +1,34 @@
+<section class="section fingertips">
+    <div class="container-md">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title text-center content-block">
+                    <h2><span class="accent-text">Trading</span> at your fingertips</h2>
+                    <p>Trade anywhere, anytime using our various platforms</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="offset-3 col-6">
+                <div class="fingertips__items d-flex justify-content-between align-items-end">
+                    <div class="fingertip-item text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/android.png" alt="">
+                        <p>Android App</p>
+                    </div>
+                    <div class="fingertip-item text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/apple.png" alt="">
+                        <p>iPhone / iPad</p>
+                    </div>
+                    <div class="fingertip-item text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/windows.png" alt="">
+                        <p>Windows</p>
+                    </div>
+                    <div class="fingertip-item text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/linux.png" alt="">
+                        <p>Linux</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
